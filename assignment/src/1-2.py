20325355f5db48750ca86165e7b5f96577e0924c
ferrolho <henriqueferrolho@gmail.com>
2016-12-03 12:21:27 +0000

    Fixes Jacobian for exercise 1.2 of HW1

--- assignment/src/1-2.py
+++ assignment/src/1-2.py
@@ -11,69 +11,94 @@ q_4 = Symbol('q_4')
 
 fvars = symbols('q_1, q_2, q_3, q_4')
 
-q = [0.66,
-	-0.44,
-	 1.06,
-	 1.33]
+q = [0.66, -0.44, 1.06, 1.33]
 
 T_b_L1 = Matrix([
-	[ cos(q_1), -sin(q_1), 0, 0],
-	[ sin(q_1),  cos(q_1), 0, 0],
+	[ cos(q_1), -sin(q_1), 0,    0],
+	[ sin(q_1),  cos(q_1), 0,    0],
 	[        0,         0, 1, 0.16],
-	[        0,         0, 0, 1]])
+	[        0,         0, 0,    1]])
 
 T_L1_L2 = Matrix([
-	[  cos(q_2), 0, sin(q_2), 0],
-	[         0, 1,        0, 0],
+	[  cos(q_2), 0, sin(q_2),    0],
+	[         0, 1,        0,    0],
 	[ -sin(q_2), 0, cos(q_2), 0.15],
-	[         0, 0,        0, 1]])
+	[         0, 0,        0,    1]])
 
 T_L2_L3 = Matrix([
-	[  cos(q_3), 0, sin(q_3), 0],
-	[         0, 1,        0, 0],
+	[  cos(q_3), 0, sin(q_3),    0],
+	[         0, 1,        0,    0],
 	[ -sin(q_3), 0, cos(q_3), 0.2],
-	[         0, 0,        0, 1]])
+	[         0, 0,        0,    1]])
 
 T_L3_L4 = Matrix([
-	[  cos(q_4), 0, sin(q_4), 0],
-	[         0, 1,        0, 0],
+	[  cos(q_4), 0, sin(q_4),    0],
+	[         0, 1,        0,    0],
 	[ -sin(q_4), 0, cos(q_4), 0.15],
-	[         0, 0,        0, 1]])
+	[         0, 0,        0,    1]])
 
 T_L4_g = Matrix([
-	[ 1, 0, 0, 0],
-	[ 0, 1, 0, 0],
+	[ 1, 0, 0,    0],
+	[ 0, 1, 0,    0],
 	[ 0, 0, 1, 0.35],
-	[ 0, 0, 0, 1]])
+	[ 0, 0, 0,    1]])
 
 T_b_g = T_b_L1 * T_L1_L2 * T_L2_L3 * T_L3_L4 * T_L4_g
+T_b_g = simplify(T_b_g)
+
+print('Transformation matrix from the base to the gripper:')
+print()
+pprint(T_b_g)
+
+print()
+print('# - - - - - - - - - - - - - - - - -')
+print()
+
+kinematicMap = Matrix([
+	[T_b_g[0, 3]],
+	[T_b_g[1, 3]],
+	[T_b_g[2, 3]]])
+
+print('Kinematic map:')
+print()
+pprint(kinematicMap)
+
+print()
+print('# - - - - - - - - - - - - - - - - -')
+print()
 
 # - - - - - - - - - - - - - - - - -
 
-dx_dq_1 = T_b_g[0].diff(q_1)
-dx_dq_2 = T_b_g[0].diff(q_2)
-dx_dq_3 = T_b_g[0].diff(q_3)
-dx_dq_4 = T_b_g[0].diff(q_4)
+dx_dq_1 = kinematicMap[0].diff(q_1)
+dx_dq_2 = kinematicMap[0].diff(q_2)
+dx_dq_3 = kinematicMap[0].diff(q_3)
+dx_dq_4 = kinematicMap[0].diff(q_4)
 
-dy_dq_1 = T_b_g[1].diff(q_1)
-dy_dq_2 = T_b_g[1].diff(q_2)
-dy_dq_3 = T_b_g[1].diff(q_3)
-dy_dq_4 = T_b_g[1].diff(q_4)
+dy_dq_1 = kinematicMap[1].diff(q_1)
+dy_dq_2 = kinematicMap[1].diff(q_2)
+dy_dq_3 = kinematicMap[1].diff(q_3)
+dy_dq_4 = kinematicMap[1].diff(q_4)
 
-dz_dq_1 = T_b_g[2].diff(q_1)
-dz_dq_2 = T_b_g[2].diff(q_2)
-dz_dq_3 = T_b_g[2].diff(q_3)
-dz_dq_4 = T_b_g[2].diff(q_4)
+dz_dq_1 = kinematicMap[2].diff(q_1)
+dz_dq_2 = kinematicMap[2].diff(q_2)
+dz_dq_3 = kinematicMap[2].diff(q_3)
+dz_dq_4 = kinematicMap[2].diff(q_4)
 
 jacobian = Matrix([
 	[dx_dq_1, dx_dq_2, dx_dq_3, dx_dq_4],
 	[dy_dq_1, dy_dq_2, dy_dq_3, dy_dq_4],
 	[dz_dq_1, dz_dq_2, dz_dq_3, dz_dq_4]])
 
-res = jacobian.evalf(subs = dict(zip(fvars, q)))
-
+print('Jacobian:')
+print()
 pprint(simplify(jacobian))
 
-print('\n\n')
+print()
+print('# - - - - - - - - - - - - - - - - -')
+print()
 
+res = jacobian.evalf(subs = dict(zip(fvars, q)))
+
+print('Jacobian at q =', q, ':')
+print()
 pprint(res)
